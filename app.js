1c86ebafdb6287b8043d390ae36583f4f6e89895
Brandon Short <brandonjshort@gmail.com>
2022-02-12 09:41:06 -0700

    changed listening port

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.use(errorController.get404);
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    app.listen(3000);
+    app.listen(5000);
   })
   .catch(err => {
     console.log(err);
